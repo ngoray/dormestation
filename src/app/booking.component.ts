8e4a9d8ed34ed399ac787639171034654d09ed71
Ray <ngorui15@gmail.com>
2019-01-30 05:49:55 +0800

    initial commit

--- src/app/booking.component.ts
+++ src/app/booking.component.ts
@@ -22,6 +22,13 @@ body {font-family: Arial, Helvetica, sans-serif;}
   opacity: 1;
 }
 
+.container1 {
+  border-radius: 5px;
+  padding: 25px; 
+  float: left;
+  background: rgba(0, 0, 0, 0.6);
+}
+
 img {
   border-radius: 10%;
 }
@@ -44,6 +51,25 @@ img {
   padding: 20px;
   color: white;
 }
+
+#modalbg1{
+  width:100%;
+  height:100vh;
+  position: absolute;
+  left: 0%;
+  top: 0%;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.6);
+}
+
+#modalbox1{
+  width:50%;
+  height:350px;
+  z-index: 70;
+  padding: 20px;
+  color: white;
+}
   .btn {
     border: none;
     color: white;
@@ -59,8 +85,39 @@ img {
     background: rgba(0, 0, 0, 0.432)!important;
     color: white!important;
   }
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
 }
 
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  top: -5px;
+  right: 105%;
+}
+
+    
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+
+.position3 {
+
+  position: absolute;
+  bottom: 7%;
+  right: 50%;
+}
 </style>
 
 <table>
@@ -78,6 +135,13 @@ img {
 </tr>
 
 </table>
+
+<div class="position3">
+<a (click)="openModal1()" class="tooltip">
+  <img src="assets/viewc.png" height="55px" width="50px"> 
+  <span class="tooltiptext">Reviews</span>
+</a>
+</div>
 
 <div id="modalbg" style="display:none">
 <br>
@@ -117,12 +181,48 @@ background: #6B78AF;"
 </tr>
 </table>
 </div>
-</div>`,
+</div>
+
+
+<div id="modalbg1" style="display:none">
+<br>
+<div id="modalbox1">
+<table>
+<tr>
+<td>
+<h2>Reviews</h2>
+</td>
+</tr>
+<tr>
+<td>
+<div class="container1" *ngFor="let quote of quotes" style="float: left; padding: 10px;">
+<h4>Name:    {{quote.name}}</h4>
+<h4>Email:   {{quote.email}}</h4>
+<h4>Rating:  {{quote.rating}}</h4>
+<h4>Comment: {{quote.comment}}</h4>
+<button
+class ="btn" 
+style="
+float: right;border: none;
+color: white;
+padding: 16px 32px;
+text-align: center;
+font-size: 16px;
+margin: 4px 2px;
+transition: 0.3s;
+background: #6B78AF;"
+(click)="closeModal1()">Cancel</button>
+</div>
+</td>
+</tr>
+</table>
+`,
 })
 export class BookingComponent implements OnInit {
 
   rooms: any = [];
   public room: any = {};
+  quotes: any[];
 
   openModal(room) {
     const modal = document.getElementById('modalbg');
@@ -141,22 +241,49 @@ export class BookingComponent implements OnInit {
 
   }
 
+  openModal1() {
+    const modal = document.getElementById('modalbg1');
+    modal.style.display = 'flex';
+
+  }
+
+  closeModal1() {
+    const modal = document.getElementById('modalbg1');
+    modal.style.display = 'none';
+
+  }
+
+  getComment(){
+    this.postsService.getComment().subscribe(quotes => {       
+      this.quotes = quotes;     
+  });
+}
+
+getRoom(){
+  this.postsService.getRoom().subscribe(rooms => {       
+    this.rooms = rooms;     
+});
+}
+
   constructor(private fb: FormBuilder, private postsService: PostsService,
     private router: Router) {  
-
-        this.postsService.getRoom().subscribe(rooms => {       
-          this.rooms = rooms;   
-
-      });
+      this.getRoom();
+      this.getComment();
     }
 
   ngOnInit() {
     window.onclick = (event) => {
       const modal = document.getElementById('modalbg');
+      const modal1 = document.getElementById('modalbg1');
       if (event.target === modal) {
         modal.style.display = "none";
+      }
+
+      else if (event.target === modal){
+        modal.style.display="none";
       }
     }
   }
 
+  
 }
