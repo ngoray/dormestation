06866df26b31b692247b0abae65419a24a495175
Ray <ngorui15@gmail.com>
2019-01-30 12:23:59 +0800

    initial commit

--- src/app/feedback.component.ts
+++ src/app/feedback.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PostsService } from './posts.service'; 
+import { PostsService } from './posts.service';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -69,17 +69,24 @@ input[type=submit]:hover {
 </style>
 
   <div class="container">
-  <form novalidate [formGroup]="myForm" (ngSubmit)="onSubmit(myForm)">
+  <form [formGroup]="myForm" (ngSubmit)="onSubmit(myForm)">
   <h1>Feedback</h1>
   <p>Please provide your feedback below:</p>
 
   <div style="float:left;" class="text">
   <p>Your Name</p>
-  <input type="text" id="name"  formControlName="name" class="text2" name="name" placeholder="Your name" required>
+  <input type="text" id="name"  formControlName="name" class="text2" name="name" placeholder="Your name" [ngClass]="{ 'is-invalid': submitted && f.name.errors }"/>
+  <div *ngIf="submitted && f.name.errors" class="invalid-feedback">
+  <div *ngIf="f.name.errors.required">name is required</div>
+</div>
   </div>
   <div style="float:left;" class="text">
   <p>Email:</p>
-  <input type="text" class="text2"  formControlName="email" id="email" name="Email" placeholder="example@gmail.com" required>
+  <input type="text" class="text2"  formControlName="email" id="email" name="Email" placeholder="example@gmail.com" [ngClass]="{ 'is-invalid': submitted && f.email.errors }"/>
+  <div *ngIf="submitted && f.email.errors" class="invalid-feedback">
+  <div *ngIf="f.email.errors.required">Email is required</div>
+  <div *ngIf="f.email.errors.email">Email must be a valid email address</div>
+  </div>
   </div>
 
   <h4>How do you rate your overall experience?</h4>
@@ -89,33 +96,42 @@ input[type=submit]:hover {
   <input formControlName="rating" type="radio" name="rating" value="good">Good <br><br>
 
   <p>Comments:</p>
-  <textarea id="comment"  formControlName="comment" name="comment" placeholder="Write something.." style="height:200px" required></textarea>
+  <textarea id="comment"  formControlName="comment" name="comment" placeholder="Write something.." style="height:200px"></textarea>
 
   <input type="submit" value="Submit">
   </form>
 </div>
 `,
 })
-export class FeedbackComponent  {
+export class FeedbackComponent {
 
   myForm: FormGroup;
-  posts: any = [];  
+  posts: any = [];
 
   constructor(private fb: FormBuilder, private authService: PostsService,
-    private router: Router) {}
+    private router: Router) { }
 
   ngOnInit() {
     this.myForm = this.fb.group({
-      name: '',
-      email: '',
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       rating: '',
-      comment:''
+      comment: '',
     });
   }
-  onSubmit() { 
+
+  get f() { return this.myForm.controls; }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      alert("Please fill in the required details");
+    }
+    else{
     console.log(this.myForm);
-    this.authService.addComment(this.myForm.value.name, 
-      this.myForm.value.email, this.myForm.value.rating, this.myForm.value.comment).subscribe(); 
-      this.router.navigateByUrl('/track'); }
-    
+    this.authService.addComment(this.myForm.value.name,
+      this.myForm.value.email, this.myForm.value.rating, this.myForm.value.comment).subscribe();
+    this.router.navigateByUrl('/track');
+    }
+  }
+
 }
